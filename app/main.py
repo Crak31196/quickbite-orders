40e9576a9b6637d7d7a38dbb6d67d47e936340b4
Rakesh Rajendra Chaudhari <73292478+Crak31196@users.noreply.github.com>
2026-09-23 10:47:34 +0530

    Add logging configuration for FastAPI app
    Set up logging configuration for the FastAPI application, including file and stream handlers.

--- app/main.py
+++ app/main.py
@@ -2,6 +2,21 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 from fastapi.staticfiles import StaticFiles
+import logging
+import time
+import random
+from fastapi import Request
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)-5s [%(name)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.FileHandler("logs/server.log"),
+        logging.StreamHandler()  # also prints to terminal
+    ]
+)
+logger = logging.getLogger("quickbite")
 
 app = FastAPI(title="QuickBite Orders API")
 
